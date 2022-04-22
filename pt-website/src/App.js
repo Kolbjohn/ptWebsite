5a5a08892f7a0b0e70721bb8359bc2f2686b6730
Kolbjohn <31744782+Kolbjohn@users.noreply.github.com>
2022-04-21 21:24:03 -0500

    fixing modules

--- pt-website/src/App.js
+++ pt-website/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select'
 
 function App() {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +23,7 @@ function App() {
         >
           Learn React
         </a>
+        <Select options={options} />
       </header>
     </div>
   );
