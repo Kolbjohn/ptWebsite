3102f252472e8ab14ccd0e9905b07269d494d447
Kolbjohn <31744782+Kolbjohn@users.noreply.github.com>
2022-05-01 16:21:41 -0500

    testing new stuff

--- server/server.js
+++ server/server.js
@@ -1,41 +1,8 @@
-const express = require("express");
-// const bodyParser = require("body-parser"); /* deprecated */
-const cors = require("cors");
-
-const path = __dirname + '/views/'
+const express = require('express');
 const app = express();
 
-app.use(express.static(path));
+app.get("/api", (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
 
-const PORT = process.env.PORT || 8080;
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(express.json());  /* bodyParser.json() is deprecated */
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
-
-const db = require("./models");
-
-db.sequelize.sync();
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
-// simple route
-app.get("/", (req, res) => {
-  res.sendFile(path + "index.html");
-});
-
-require("./routes/exercise.routes")(app);
-
-// set port, listen for requests
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+app.listen(5000, () => console.log("Server started on port 5000"))
